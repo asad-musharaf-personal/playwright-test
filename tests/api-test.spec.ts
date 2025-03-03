f2dced0db64158bbd64877a2e46c8fe12c28d205
Asad <asad.musharaf@devigital.com>
2025-03-04 01:12:38 +0500

    first commit

---
+++ tests/api-test.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+
+test('api get test', async ({ request }) => {
+
+    const response = await request.get('https://reqres.in/api/users?page=2')
+
+    expect(response.status()).toBe(200)
+
+    const text = await response.text();
+
+    expect(text).toContain('tobias.funke@reqres.in')
+
+
+});
+
+test.only('api post test', async ({ request }) => {
+
+    const response = await request.post('https://reqres.in/api/register', {
+
+        data: {
+            "email": "eve.holt@reqres.in",
+            "password": "pistol"
+        }
+    }
+    )
+
+    expect(response.status()).toBe(200)
+
+    const responseBody = await response.json();
+
+    expect(responseBody).toHaveProperty('id', 4);
+    expect(responseBody).toHaveProperty('token', 'QpwL5tke4Pnpja7X4');
+
+});
